955b8fee1918ccb8f604e4e1603f123a133f4333
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 20:03:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let i = 0
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.addEventListener('keydown', function(e){
+     const key = parseInt(e.detail || e.which);
+      if(key === code[i]){
+           i++
+         }
+    if(i === code.length )  {
+        alert('Hurray!')
+        } else {
+          i = 0
+        }
+})
 }
