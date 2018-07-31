cf373477410df21cc43dc56c5f5b6cef074829a6
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 14:22:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let i = 0;
 function init() {
+  if(i === code.length-1 )  {
+      alert('Hurray!')
+    }
 }
 document.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.which);
@@ -12,7 +15,5 @@ document.addEventListener('keydown', function(e){
           i = 0
         }
 
-        if(i === code.length-1 )  {
-            alert('Hurray!')
-          }
+        
 })
