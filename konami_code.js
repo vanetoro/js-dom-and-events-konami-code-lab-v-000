f0cea87ab7e833cfa1dea82800d298b7d13b0916
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 15:10:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
       let i = 0;
   document.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
-    console.log(key)
+    console.log(key, i)
 
         if(key === code[i]){
               i++;
