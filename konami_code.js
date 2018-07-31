e363d0560df2bb738f2f74d30d4aa0ec65f4c975
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 14:31:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let i = 0;
-function init(){
+
 document.addEventListener('keydown', function(e){
+  function init(){
   const key = parseInt(e.detail || e.which);
   console.log(key, i, code.length)
       if(key === code[i]){
@@ -12,5 +13,5 @@ document.addEventListener('keydown', function(e){
       if(i === code.length )  {
           alert('Hurray!')
           }
+          }
 })
-}
