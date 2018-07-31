47128792798373ced5dcf377bfdf3813567b6615
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 14:42:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let i = 0;
   function init(e){
     console.log(e)
-    const key = parseInt( e.which);
+    const key = parseInt( e.keyCode);
     console.log(key)
         if(key === code[i]){
             i++;
