ac4d2f4fc9520bc499f4f0fa28be6046df62983b
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 13:58:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,16 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let i = 0
 function init() {
   // Write your JavaScript code inside the init() function
-  document.addEventListener('keydown', function(e){
-     const key = parseInt(e.detail || e.which);
-      if(key === code[i]){
-        console.log(key)
-           i++
-         }
-    if(i === code.length )  {
-        alert('Hurray!')
-        } else {
-          i = 0
-        }
-})
+//   document.addEventListener('keydown', function(e){
+//      const key = parseInt(e.detail || e.which);
+//       if(key === code[i]){
+//         console.log(key)
+//            i++
+//          }
+//     if(i === code.length )  {
+//         alert('Hurray!')
+//         } else {
+//           i = 0
+//         }
+// })
+// }
+document.addEventListener('keydown', function(e){
+  console.log(e)
 }
