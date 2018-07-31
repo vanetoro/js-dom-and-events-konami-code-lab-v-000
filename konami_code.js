5516b430f338805afc3f3c5f8b2bdaabe34e794e
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 14:33:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let i = 0;
-  function init(){
+  function init(e){
 
     const key = parseInt(e.detail || e.which);
     console.log(key, i, code.length)
