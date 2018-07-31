569d10eeb2754e6e43b55b52daf9b96c46457719
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 14:44:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let i = 0;
-  function init(e){
-    console.log(e)
+  function init(){
+    console.log()
     const key = e.detail || e.which;
     console.log(key)
         if(key === code[i]){
